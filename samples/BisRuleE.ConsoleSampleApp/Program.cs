2affc07397ec1f6891ec2e82c59fff1f0f4243f5
Jos Menhart <Jos@estreme.eu>
2020-07-11 20:21:15 +0200

    yfgujf
    fgdfs

---
+++ samples/BisRuleE.ConsoleSampleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using BisRuleE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBREConsoleSampleApp
+{
+    internal partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            var customer = new Customer() { Name = "Jos", Balance = 100M };
+
+            var rule = new IBRERule(customer, "Name", IBREComparisonOperator.Equals, "Jos");
+
+            var result = rule.Assert();
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
